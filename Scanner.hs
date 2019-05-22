b52f57c27c036060e11c184177aaca8d621254b9
Brent Yorgey <byorgey@gmail.com>
2019-05-22 11:57:59 -0500

    initial import of Scanner.hs

---
+++ Scanner.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE LambdaCase #-}
+
+import           Control.Monad.State
+
+------------------------------------------------------------
+
+type Scanner = State [String]
+
+runScanner :: Scanner a -> String -> a
+runScanner = runScannerWith words
+
+runScannerWith :: (String -> [String]) -> Scanner a -> String -> a
+runScannerWith t s = evalState s . t
+
+str :: Scanner String
+str = get >>= \case { s:ss -> put ss >> return s }
+
+int :: Scanner Int
+int = read <$> str
+
+integer :: Scanner Integer
+integer = read <$> str
+
+double :: Scanner Double
+double = read <$> str
+
+numberOf :: Scanner a -> Scanner [a]
+numberOf s = int >>= flip replicateM s
+
+many :: Scanner a -> Scanner [a]
+many s = get >>= \case { [] -> return []; _ -> (:) <$> s <*> many s }
