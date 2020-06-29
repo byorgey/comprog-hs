04b93f75ace1ae84faf9a774ca03ceb9aa0dbc4d
Brent Yorgey <byorgey@gmail.com>
2020-06-29 16:35:41 -0500

    new utility functions: sortGroupOn, pairs, and withPairs

--- Util.hs
+++ Util.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE TupleSections #-}
+
 module Util where
 
+import           Control.Arrow
+import           Data.Function
+import           Data.List
 import           Data.Maybe
+import           Data.Ord
 
 fi :: (Integral a, Num b) => a -> b
 fi = fromIntegral
@@ -10,3 +16,19 @@ fj = fromJust
 
 both :: (a -> b) -> (a,a) -> (b,b)
 both f (x,y) = (f x, f y)
+
+sortGroupOn :: Ord b => (a -> b) -> [a] -> [(b,[a])]
+sortGroupOn f = sortBy (comparing f) >>> groupBy ((==) `on` f) >>> map ((f.head) &&& id)
+
+pairs :: [a] -> [(a,a)]
+pairs []     = []
+pairs [_]    = []
+pairs (a:as) = map (a,) as ++ pairs as
+
+withPairs :: Monoid r => (a -> a -> r) -> [a] -> r
+withPairs _ []     = mempty
+withPairs _ [_]    = mempty
+withPairs f (a:as) = go as
+  where
+    go []        = withPairs f as
+    go (a2:rest) = f a a2 <> go rest
