d73e514fd8ba8cc904f42585a8755cb45f1fee68
Brent Yorgey <byorgey@gmail.com>
2021-01-08 14:43:43 -0600

    Geom: add dot, perp, and some 2D line stuff

--- Geom.hs
+++ Geom.hs
@@ -26,13 +26,18 @@ V2 x1 y1 ^-^ V2 x2 y2 = V2 (x1-x2) (y1-y2)
 (*^) :: Num s => s -> V2 s -> V2 s
 k *^ (V2 x y) = V2 (k*x) (k*y)
 
+dot :: Num s => V2 s -> V2 s -> s
+dot (V2 x1 y1) (V2 x2 y2) = x1*x2 + y1*y2
+
 ------------------------------------------------------------
 -- Utilities
 
--- | This combinator allows us to write e.g. 'v2 int' or 'v2 double'
+-- | These combinators allows us to write e.g. 'v2 int' or 'v2 double'
 --   to get a 'Scanner (V2 s)'.
-v2 :: Applicative f => f s -> f (V2 s)
+v2, p2 :: Applicative f => f s -> f (V2 s)
 v2 s = V2 <$> s <*> s
+
+p2 = v2
 
 ------------------------------------------------------------
 -- Angles
@@ -59,6 +64,9 @@ fromPolar r θ = rot θ (V2 r 0)
 -- | Rotate a vector counterclockwise by a given angle.
 rot :: Angle -> V2D -> V2D
 rot (A θ) (V2 x y) = V2 (cos θ * x - sin θ * y) (sin θ * x + cos θ * y)
+
+perp :: Num s => V2 s -> V2 s
+perp (V2 x y) = V2 (-y) x
 
 ------------------------------------------------------------
 -- Cross product
@@ -101,3 +109,35 @@ signedPolyArea pts = sum $ zipWith (signedTriArea zero) pts (tail pts ++ [head p
 -- | The (nonnegative) area of the polygon with the given vertices.
 polyArea :: Fractional s => [P2 s] -> s
 polyArea = abs . signedPolyArea
+
+------------------------------------------------------------
+-- 2D Lines
+
+data L2 s = L2 { getDirection :: !(V2 s), getOffset :: !s }
+type L2D = L2 Double
+
+lineFromEquation :: Num s => s -> s -> s -> L2 s
+lineFromEquation a b c = L2 (V2 b (-a)) c
+
+lineFromPoints :: Num s => P2 s -> P2 s -> L2 s
+lineFromPoints p q = L2 v (v `cross` p)
+  where
+    v = q ^-^ p
+
+side :: Num s => L2 s -> P2 s -> s
+side (L2 v c) p = cross v p - c
+
+leftOf :: (Num s, Ord s) => L2 s -> P2 s -> Bool
+leftOf l p = side l p > 0
+
+rightOf :: (Num s, Ord s) => L2 s -> P2 s -> Bool
+rightOf l p = side l p < 0
+
+toProjection :: Fractional s => L2 s -> P2 s -> V2 s
+toProjection l@(L2 v _) p = (-side l p / (dot v v)) *^ perp v
+
+project :: Fractional s => L2 s -> P2 s -> P2 s
+project l p = p ^+^ toProjection l p
+
+reflectAcross :: Fractional s => L2 s -> P2 s -> P2 s
+reflectAcross l p = p ^+^ (2 *^ toProjection l p)
