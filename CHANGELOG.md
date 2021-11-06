ee885f3c1e1f7c5ff856c0e3db2bd027a1a3672e
Brent Yorgey <byorgey@gmail.com>
2021-11-05 21:18:24 -0500

    cabalize, and add Graph.hs

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for comprog-hs
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Graph.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Graph where
+
+import           Control.Arrow       ((>>>))
+import           Control.Monad
+import           Control.Monad.ST
+import           Data.Array.IArray   (IArray, listArray)
+import qualified Data.Array.IArray   as IA
+import           Data.Array.ST
+import           Data.Array.Unboxed  (UArray)
+import qualified Data.Array.Unboxed  as U
+import           Data.Array.Unsafe   (unsafeFreeze)
+import           Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HM
+import           Data.Hashable
+import           Data.Sequence       (Seq (..), ViewL (..), (<|), (|>))
+import qualified Data.Sequence       as Seq
+import           Data.Set            (Set)
+import qualified Data.Set            as S
+
+------------------------------------------------------------
+-- Utilities
+------------------------------------------------------------
+
+infixl 0 >$>
+(>$>) :: a -> (a -> b) -> b
+(>$>) = flip ($)
+{-# INLINE (>$>) #-}
+
+exhaustM :: Monad m => (a -> m (Maybe a)) -> a -> m a
+exhaustM f = go
+  where
+    go a = do
+      ma <- f a
+      maybe (return a) go ma
+
+------------------------------------------------------------
+-- Indexing
+------------------------------------------------------------
+
+data Indexing v = Indexing { size :: Int, toInt :: v -> Int, fromInt :: Int -> v }
+
+idIndexing :: Int -> Indexing Int
+idIndexing n = Indexing n id id
+
+indexingFrom :: forall v. (IArray UArray v, Hashable v, Ord v) => [v] -> Indexing v
+indexingFrom vs = Indexing n (fromV HM.!) (toV IA.!)
+  where
+    vs' = S.toList . S.fromList $ vs
+    n = length vs'
+    toV :: UArray Int v
+    toV = listArray (0,n-1) vs'
+    fromV :: HashMap v Int
+    fromV = HM.fromList (zip vs' [0 :: Int ..])
+
+------------------------------------------------------------
+-- BFS
+------------------------------------------------------------
+
+data BFSResult v =
+  BFSR { getLevel :: v -> Maybe Int, getParent :: v -> Maybe v }
+
+type V = Int
+data BFSState s =
+  BS { level :: STUArray s V Int, parent :: STUArray s V V, queue :: Seq V }
+
+initBFSState :: Int -> [Int] -> ST s (BFSState s)
+initBFSState n vs = do
+  l <- newArray (0,n-1) (-1)
+  p <- newArray (0,n-1) (-1)
+
+  forM_ vs $ \v -> writeArray l v 0
+  return $ BS l p (Seq.fromList vs)
+
+bfs :: forall v. (Eq v, Hashable v) => Indexing v -> [v] -> (v -> [v]) -> (v -> Bool) -> BFSResult v
+bfs Indexing{..} vs next goal
+  = toResult $ bfs' size (map toInt vs) (map toInt . next . fromInt) (goal . fromInt)
+  where
+    toResult :: (forall s. ST s (BFSState s)) -> BFSResult v
+    toResult m = runST $ do
+      st <- m
+      (level' :: UArray V Int) <- unsafeFreeze (level st)
+      (parent' :: UArray V V) <- unsafeFreeze (parent st)
+      return $
+        BFSR
+          ((\l -> if l == -1 then Nothing else Just l) . (level' IA.!) . toInt)
+          ((\p -> if p == -1 then Nothing else Just (fromInt p)) . (parent' IA.!) . toInt)
+
+visited :: BFSState s -> V -> ST s Bool
+visited BS{..} v = (/= -1) <$> readArray level v
+{-# INLINE visited #-}
+
+bfs' :: Int -> [V] -> (V -> [V]) -> (V -> Bool) -> ST s (BFSState s)
+bfs' n vs next goal = do
+  st <- initBFSState n vs
+  exhaustM bfsStep st
+  where
+    bfsStep st@BS{..} = case Seq.viewl queue of
+      EmptyL -> return Nothing
+      v :< q'
+        | goal v -> return Nothing
+        | otherwise -> v >$> next >>> filterM (fmap not . visited st)
+            >=> foldM (upd v) (st{queue=q'}) >>> fmap Just
+
+    upd p b@BS{..} v = do
+      lp <- readArray level p
+      writeArray level v (lp + 1)
+      writeArray parent v p
+      return $ b{queue = queue |> v}
