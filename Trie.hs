ead48cd14c16c5150d4bd70d457121bdaeaaee4f
Brent Yorgey <byorgey@gmail.com>
2023-05-03 14:39:46 -0500

    a couple more small improvements to Trie

--- Trie.hs
+++ Trie.hs
@@ -23,8 +23,8 @@ insert :: C.ByteString -> a -> Trie a -> Trie a
 insert w a t = fst (go w t)
   where
     go = C.foldr
-      (\c ins (Trie n v m) ->
-         let (t', ds) = ins (fromMaybe emptyTrie (M.lookup c m))
+      (\c insSuffix (Trie n v m) ->
+         let (t', ds) = insSuffix (fromMaybe emptyTrie (M.lookup c m))
          in  (Trie (n+ds) v (M.insert c t' m), ds)
       )
       (\(Trie n v m) ->
@@ -52,15 +52,14 @@ lookup = C.foldr ((>=>) . lookup1) value
 foldTrie :: (Int -> Maybe a -> Map Char r -> r) -> Trie a -> r
 foldTrie f (Trie n b m) = f n b (M.map (foldTrie f) m)
 
--- | "Drive" a trie by repeatedly looking up consecutive characters
---   from a string, restarting at the root every time we "fall off"
---   the bottom, and returning the list of all values encountered
---   along the way.  Note that this function will crash if it ever
---   looks up a character which is not in the current trie.
---
---   This is of particular use in decoding using a prefix-free code.
-drive :: Trie a -> C.ByteString -> [a]
-drive t = reverse . snd . C.foldl' step (t, [])
+-- | "Decode" a string by repeatedly looking up consecutive
+--   characters. Every time we find a key which exists in the trie,
+--   emit the corresponding value and restart at the root.  This is of
+--   particular use in decoding a prefix-free code.  Note that this
+--   function will crash if it ever looks up a character which is not
+--   in the current trie.
+decode :: Trie a -> C.ByteString -> [a]
+decode t = reverse . snd . C.foldl' step (t, [])
   where
     step (s, as) c =
       let Just s' = lookup1 c s
