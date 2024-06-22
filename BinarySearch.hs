8a5a398cb8dd318748cb49823ff26df0c7700542
Brent Yorgey <byorgey@gmail.com>
2024-06-22 15:00:06 -0500

    formatting

--- BinarySearch.hs
+++ BinarySearch.hs
@@ -4,9 +4,8 @@
 
 module BinarySearch where
 
-import           Data.Bits
-import           Data.Word     (Word64)
-import           Unsafe.Coerce (unsafeCoerce)
+import Data.Bits
+import Data.Word (Word64)
 
 -- | Generic search function.  Takes a step function, a predicate, and
 --   low and high values, and it finds values (l,r) right next to each
@@ -33,41 +32,41 @@ import           Unsafe.Coerce (unsafeCoerce)
 --     - Returns (l,r) such that:
 --       - @not (p l) && p r@
 --       - @mid l r == Nothing@
-search :: (a -> a -> Maybe a) -> (a -> Bool) -> a -> a -> (a,a)
+search :: (a -> a -> Maybe a) -> (a -> Bool) -> a -> a -> (a, a)
 search mid p = go
-  where
-    go l r = case mid l r of
-      Nothing -> (l,r)
-      Just m
-        | p m       -> go l m
-        | otherwise -> go m r
+ where
+  go l r = case mid l r of
+    Nothing -> (l, r)
+    Just m
+      | p m -> go l m
+      | otherwise -> go m r
 
 -- | Step function for binary search over an integral type.  Stops
 --   when @r - l <= 1@; otherwise returns their midpoint.
 binary :: Integral a => a -> a -> Maybe a
 binary l r
-  | r - l > 1 = Just (l + (r-l) `div` 2)
+  | r - l > 1 = Just (l + (r - l) `div` 2)
   | otherwise = Nothing
 
 -- | Step function for continuous binary search.  Stops once
 --   @r - l <= eps@; otherwise returns their midpoint.
 continuous :: (Fractional a, Ord a) => a -> a -> a -> Maybe a
 continuous eps l r
-  | r - l > eps = Just (l + (r-l)/2)
+  | r - l > eps = Just (l + (r - l) / 2)
   | otherwise = Nothing
 
 -- | Step function for forward linear search.  Stops when @r - l <=
 --   1$; otherwise returns @l + 1@.
 fwd :: (Num a, Ord a) => a -> a -> Maybe a
 fwd l r
-  | r - l > 1 = Just (l+1)
+  | r - l > 1 = Just (l + 1)
   | otherwise = Nothing
 
 -- | Step function for backward linear search.  Stops when @r - l <=
 --   1$; otherwise returns @r - 1@.
 bwd :: (Num a, Ord a) => a -> a -> Maybe a
 bwd l r
-  | r - l > 1 = Just (r-1)
+  | r - l > 1 = Just (r - 1)
   | otherwise = Nothing
 
 ------------------------------------------------------------
@@ -89,19 +88,19 @@ floating l r = b2f <$> binary (f2b l) (f2b r)
 --   @x < y@ iff @f2b x < f2b y@.  'b2f' is its inverse.
 f2b :: Double -> Word64
 f2b 0 = 0x7fffffffffffffff
-f2b x
-  = (if m < 0 then 0 else bit 63)
-  `xor` flipNeg (eBits `xor` mBits)
-  where
-    (m, e) = decodeFloat x
-    eBits = fromIntegral (e + d + bias) `shiftL` d
-    mBits = fromIntegral (abs m) `clearBit` d
+f2b x =
+  (if m < 0 then 0 else bit 63)
+    `xor` flipNeg (eBits `xor` mBits)
+ where
+  (m, e) = decodeFloat x
+  eBits = fromIntegral (e + d + bias) `shiftL` d
+  mBits = fromIntegral (abs m) `clearBit` d
 
-    d = floatDigits x - 1
-    bias = 1023
-    flipNeg
-      | m < 0 = (`clearBit` 63) . complement
-      | otherwise = id
+  d = floatDigits x - 1
+  bias = 1023
+  flipNeg
+    | m < 0 = (`clearBit` 63) . complement
+    | otherwise = id
 
 prop_f2b_monotonic :: Double -> Double -> Bool
 prop_f2b_monotonic x y = (x < y) == (f2b x < f2b y)
@@ -113,14 +112,14 @@ prop_f2b_monotonic x y = (x < y) == (f2b x < f2b y)
 b2f :: Word64 -> Double
 b2f 0x7fffffffffffffff = 0
 b2f w = encodeFloat m (fromIntegral e - bias - d)
-  where
-    s = testBit w 63
-    w' = (if s then id else complement) w
+ where
+  s = testBit w 63
+  w' = (if s then id else complement) w
 
-    d = floatDigits (1 :: Double) - 1
-    bias = 1023
-    m = (if s then id else negate) (fromIntegral ((w' .&. ((1 `shiftL` d) - 1)) `setBit` d))
-    e = clearBit w' 63 `shiftR` d
+  d = floatDigits (1 :: Double) - 1
+  bias = 1023
+  m = (if s then id else negate) (fromIntegral ((w' .&. ((1 `shiftL` d) - 1)) `setBit` d))
+  e = clearBit w' 63 `shiftR` d
 
 prop_b2f_f2b :: Double -> Bool
 prop_b2f_f2b x = b2f (f2b x) == x
@@ -131,21 +130,21 @@ prop_b2f_f2b x = b2f (f2b x) == x
 -- differ by 1.
 prop_f2b_b2f :: Word64 -> Bool
 prop_f2b_b2f w = isInfinite x || isNaN x || dist (f2b x) w <= 1
-  where
-    x = b2f w
-    dist x y
-      | x < y = y - x
-      | otherwise = x - y
+ where
+  x = b2f w
+  dist x y
+    | x < y = y - x
+    | otherwise = x - y
 
 -- Given two distinct Double values, if we take the midpoint of their
 -- corresponding Word64 values, we get another Word64 that represents
 -- a floating point number strictly in between the original two.
 prop_f2b_mid_monotonic :: Double -> Double -> Bool
 prop_f2b_mid_monotonic x y = x == y || (x' < z && z < y')
-  where
-    x' = min x y
-    y' = max x y
-    l = f2b x'
-    r = f2b y'
-    m = l + (r-l) `div` 2
-    z = b2f m
+ where
+  x' = min x y
+  y' = max x y
+  l = f2b x'
+  r = f2b y'
+  m = l + (r - l) `div` 2
+  z = b2f m
